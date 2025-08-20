3d1044eb7c52df28361c60debbcd4a42626a47fb
Ayşenur Eşsiz <92052629+aysenuressiz@users.noreply.github.com>
2025-08-20 14:50:09 +0300

    Create il.sql

---
+++ il.sql
@@ -0,0 +1,91 @@
+-- Tablo Oluşturma (id otomatik artan olarak ayarlandı)
+
+CREATE TABLE Cities (
+    city_id INT PRIMARY KEY AUTO_INCREMENT,
+    city_name VARCHAR(100) NOT NULL UNIQUE
+);
+
+
+-- Veri Ekleme (alfabetik)
+
+INSERT INTO Cities (city_name) VALUES ('ADANA');
+INSERT INTO Cities (city_name) VALUES ('ADIYAMAN');
+INSERT INTO Cities (city_name) VALUES ('AFYONKARAHİSAR');
+INSERT INTO Cities (city_name) VALUES ('AĞRI');
+INSERT INTO Cities (city_name) VALUES ('AKSARAY');
+INSERT INTO Cities (city_name) VALUES ('AMASYA');
+INSERT INTO Cities (city_name) VALUES ('ANKARA');
+INSERT INTO Cities (city_name) VALUES ('ANTALYA');
+INSERT INTO Cities (city_name) VALUES ('ARDAHAN');
+INSERT INTO Cities (city_name) VALUES ('ARTVİN');
+INSERT INTO Cities (city_name) VALUES ('AYDIN');
+INSERT INTO Cities (city_name) VALUES ('BALIKESİR');
+INSERT INTO Cities (city_name) VALUES ('BARTIN');
+INSERT INTO Cities (city_name) VALUES ('BATMAN');
+INSERT INTO Cities (city_name) VALUES ('BAYBURT');
+INSERT INTO Cities (city_name) VALUES ('BİLECİK');
+INSERT INTO Cities (city_name) VALUES ('BİNGÖL');
+INSERT INTO Cities (city_name) VALUES ('BİTLİS');
+INSERT INTO Cities (city_name) VALUES ('BOLU');
+INSERT INTO Cities (city_name) VALUES ('BURDUR');
+INSERT INTO Cities (city_name) VALUES ('BURSA');
+INSERT INTO Cities (city_name) VALUES ('ÇANAKKALE');
+INSERT INTO Cities (city_name) VALUES ('ÇANKIRI');
+INSERT INTO Cities (city_name) VALUES ('ÇORUM');
+INSERT INTO Cities (city_name) VALUES ('DENİZLİ');
+INSERT INTO Cities (city_name) VALUES ('DİYARBAKIR');
+INSERT INTO Cities (city_name) VALUES ('DÜZCE');
+INSERT INTO Cities (city_name) VALUES ('EDİRNE');
+INSERT INTO Cities (city_name) VALUES ('ELAZIĞ');
+INSERT INTO Cities (city_name) VALUES ('ERZİNCAN');
+INSERT INTO Cities (city_name) VALUES ('ERZURUM');
+INSERT INTO Cities (city_name) VALUES ('ESKİŞEHİR');
+INSERT INTO Cities (city_name) VALUES ('GAZİANTEP');
+INSERT INTO Cities (city_name) VALUES ('GİRESUN');
+INSERT INTO Cities (city_name) VALUES ('GÜMÜŞHANE');
+INSERT INTO Cities (city_name) VALUES ('HAKKARİ');
+INSERT INTO Cities (city_name) VALUES ('HATAY');
+INSERT INTO Cities (city_name) VALUES ('IĞDIR');
+INSERT INTO Cities (city_name) VALUES ('ISPARTA');
+INSERT INTO Cities (city_name) VALUES ('İSTANBUL');
+INSERT INTO Cities (city_name) VALUES ('İZMİR');
+INSERT INTO Cities (city_name) VALUES ('KAHRAMANMARAŞ');
+INSERT INTO Cities (city_name) VALUES ('KARABÜK');
+INSERT INTO Cities (city_name) VALUES ('KARAMAN');
+INSERT INTO Cities (city_name) VALUES ('KARS');
+INSERT INTO Cities (city_name) VALUES ('KASTAMONU');
+INSERT INTO Cities (city_name) VALUES ('KAYSERİ');
+INSERT INTO Cities (city_name) VALUES ('KIRIKKALE');
+INSERT INTO Cities (city_name) VALUES ('KIRKLARELİ');
+INSERT INTO Cities (city_name) VALUES ('KIRŞEHİR');
+INSERT INTO Cities (city_name) VALUES ('KİLİS');
+INSERT INTO Cities (city_name) VALUES ('KOCAELİ');
+INSERT INTO Cities (city_name) VALUES ('KONYA');
+INSERT INTO Cities (city_name) VALUES ('KÜTAHYA');
+INSERT INTO Cities (city_name) VALUES ('MALATYA');
+INSERT INTO Cities (city_name) VALUES ('MANİSA');
+INSERT INTO Cities (city_name) VALUES ('MARDİN');
+INSERT INTO Cities (city_name) VALUES ('MERSİN');
+INSERT INTO Cities (city_name) VALUES ('MUĞLA');
+INSERT INTO Cities (city_name) VALUES ('MUŞ');
+INSERT INTO Cities (city_name) VALUES ('NEVŞEHİR');
+INSERT INTO Cities (city_name) VALUES ('NİĞDE');
+INSERT INTO Cities (city_name) VALUES ('ORDU');
+INSERT INTO Cities (city_name) VALUES ('OSMANİYE');
+INSERT INTO Cities (city_name) VALUES ('RİZE');
+INSERT INTO Cities (city_name) VALUES ('SAKARYA');
+INSERT INTO Cities (city_name) VALUES ('SAMSUN');
+INSERT INTO Cities (city_name) VALUES ('SİİRT');
+INSERT INTO Cities (city_name) VALUES ('SİNOP');
+INSERT INTO Cities (city_name) VALUES ('SİVAS');
+INSERT INTO Cities (city_name) VALUES ('ŞANLIURFA');
+INSERT INTO Cities (city_name) VALUES ('ŞIRNAK');
+INSERT INTO Cities (city_name) VALUES ('TEKİRDAĞ');
+INSERT INTO Cities (city_name) VALUES ('TOKAT');
+INSERT INTO Cities (city_name) VALUES ('TRABZON');
+INSERT INTO Cities (city_name) VALUES ('TUNCELİ');
+INSERT INTO Cities (city_name) VALUES ('UŞAK');
+INSERT INTO Cities (city_name) VALUES ('VAN');
+INSERT INTO Cities (city_name) VALUES ('YALOVA');
+INSERT INTO Cities (city_name) VALUES ('YOZGAT');
+INSERT INTO Cities (city_name) VALUES ('ZONGULDAK');
